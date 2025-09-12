295855d8affd0376fe03844ef5b9e63a5bde661e
kennzuki <kingsyken@gmail.com>
2025-09-13 02:42:50 +0300

    context provider for product

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { useEffect, useState } from 'react';
-import ProductCard from "./components/ProductCard";
-import ProductList from "./components/ProductList";
-
+import ProductList from './components/ProductList';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await fetch('/api/products');
-        if (!res.ok) throw new Error('Failed to fetch products');
-        const data = await res.json();
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
   return (
     <div className='max-w h-screen p-16 space-y-6'>
-      <h1 className='text-3xl text-green-500 uppercase font-bold'>Kenki shoppng cart</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error} No data was fetched</p>}
-     <ProductList products={products} />
+      <h1 className='text-3xl text-green-500 uppercase font-bold'>
+        Kenki shoppng cart
+      </h1>
+
+      <ProductList />
     </div>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import { ProductContext} from "../context/ProductContext";
 import ProductCard from './ProductCard';
 
-ProductCard;
-const ProductList = ({ products }) => {
+
+const ProductList = () => {
+    const{products,loading,error}=useContext(ProductContext);
   return (
-    <div className='grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 shadow-2xl p-4'>
+      <div className='grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 shadow-2xl p-4'>
+          {loading && <p>Loading...</p>}
+          {error && <p className="text-red-500 text-center" >{error}</p>}
       {products.map((product) => {
         return <ProductCard key={product.id} product={product} />;
       })}
